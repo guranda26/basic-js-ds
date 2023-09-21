a210e3b29537f95241dd261cd7b3353103daa060
gurandalemonjava <guralemo@gmail.com>
2023-09-21 19:35:46 +0400

    .

--- Desktop/myprojectapp 2/src/components/ContactForm.vue
+++ Desktop/myprojectapp 2/src/components/ContactForm.vue
@@ -2,20 +2,32 @@
   <div>
     <form>
       <div>
-        <input type="email" placeholder="" />
+        <input v-model="email" type="email" placeholder="email" />
       </div>
       <div>
-        <textarea></textarea>
+        <textarea v-model="message"></textarea>
       </div>
       <div>
-        <button>{{ buttonText }}</button>
+        <button type="button" @click="sendMessage()">{{ buttonText }}</button>
       </div>
     </form>
   </div>
 </template>
 <script>
 export default {
-  props: [`buttonText`],
-  //buttonText: String
+  props: {
+    buttonText: String,
+  },
+  data() {
+    return {
+      email: null,
+      message: null,
+    };
+  },
+  methods: {
+    sendMessage() {
+      console.log(this.email, this.message);
+    },
+  },
 };
 </script>
